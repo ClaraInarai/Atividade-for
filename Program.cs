b922982d1c02d8d21d4246b0730a515343309297
Clara <aralcinarai@gmail.com>
2019-06-08 14:26:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._2__clara_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("informe um numero: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= n; i += 2)
+            {
+                Console.WriteLine(i);
+
+            }
+          
+            Console.ReadKey();
+        }
+    }
+}
